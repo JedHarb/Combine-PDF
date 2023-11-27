a952fa1fc3e01f7ae7e323d0a879fdc5e9f74ee8
Jared Harber <35547112+JedHarb@users.noreply.github.com>
2023-11-27 16:45:53 -0600

    Update Combine-PDF.ps1

--- Combine-PDF.ps1
+++ Combine-PDF.ps1
@@ -1,36 +1,35 @@
-if (Test-Path "C:\Program Files\gs\gswin64c.exe") {
-	"Combine all PDFs in this folder to a single Combined.pdf"
-	"Press any key to proceed."
-	[Console]::ReadKey() > $null
-	$PDFs = Get-ChildItem -File -Include *.pdf
- 
-	if ($PDFs) {
+if ($GS = Get-ChildItem -Path "C:\Program Files\gs\" -Recurse -Include gswin*.exe | Sort-Object LastWriteTime -Descending | Select-Object -First 1) {
+	if ($PDFs = Get-ChildItem -Path .\* -File -Include *.pdf) {
+		"Ready to combine all PDFs in this folder to a single Combined.pdf"
+		"Press any key to review."
+		[Console]::ReadKey() > $null
+		
 		if (Test-Path "Combined.pdf") {
-			"You already have a Combined.pdf here. Please delete or move it and try again."
+			"`nYou already have a Combined.pdf here. Please delete or move it and try again."
 		}
 		else {
 			# Save the full file paths with quotation marks around each one so Ghostscript can handle spaces.
 			$PDFPaths = $PDFs | ForEach-Object {"`"$_`""}
 			
 			$OFS = "`r`n" # Output Field Separator. Preserve newlines when an array is expressed as a string.
-			"The PDFs to combine will be in this order:`n"
+			"`nThe PDFs to combine will be in this order:`n"
 			$PDFs.Name
 			"`nPress any key to combine now, or exit this script to cancel."
 			[Console]::ReadKey() > $null
 			
 			# Use Ghostscript via CMD to merge all PDFs into a single one.
-			& "C:\Program Files\gs\gswin64c.exe" -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile="Combined.pdf" $PDFPaths | Out-Default
+			& $GS -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile="Combined.pdf" $PDFPaths | Out-Default
 				
 			if ((Get-Item .\Combined.pdf).length -lt 5000) { # If Ghostscript fails to merge, it will output a very tiny (less than 5kb) pdf instead.
    				Write-Host "PDF merge failed! You may have a corrupted PDF in this folder." -ForegroundColor 'Red'
 				Remove-Item .\Combined.pdf
 			}
 			else {
-				"Finished"
+				"`nCombined!"
 				if ((Read-Host "If you want to delete ALL source PDFs now, please enter the letter 'Y'") -eq "Y") {
 					$PDFs | Remove-Item # Delete the source PDFs after merge.
-     					"Deleted"
-     				}
+					"Deleted"
+				}
 			}
 		}
 	}
@@ -39,7 +38,8 @@ if (Test-Path "C:\Program Files\gs\gswin64c.exe") {
 	}
 }
 else {
-	"Could not locate C:\Program Files\gs\gswin64c.exe"
+	"Could not locate an exe anywhere inside C:\Program Files\gs\"
 	"Please install Ghostscript and make sure this file exists (or edit this script to point to the Ghostscript exe)."
 }
+"Finished. Press any key to exit."
 [Console]::ReadKey() > $null # Pause script (waiting for any user input) so any of the above messages don't immediately close.
